8c26db64534fca062c230bac0c167a83cd440c87
lalajie <15862811804@163.com>
2020-10-01 17:03:07 +0800

    10-1

---
+++ src/com/soft2036/unit2/Example2_9.java
@@ -0,0 +1,14 @@
+package com.soft2036.unit2;
+
+/**
+ * @author Mister-Lu
+ */
+public class Example2_9{
+    public static void main(String[] args){
+        Weekday x=Weekday.星期日;
+        if(x==Weekday.星期日) {
+            System.out.println(x);
+            System.out.println("今天我休息");
+        }
+    }
+}
---
+++ src/com/soft2036/unit2/Weekday.java
@@ -0,0 +1,9 @@
+package com.soft2036.unit2;
+
+/**
+ * @author Mister-Lu
+ */
+
+public enum Weekday {
+    星期一, 星期二, 星期三, 星期四, 星期五, 星期六, 星期日
+}
