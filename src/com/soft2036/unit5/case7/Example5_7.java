807f857d19af5977aa57305f5b5472d30a84130e
lalajie <15862811804@163.com>
2020-10-12 13:22:03 +0800

    本地仓提交

--- src/com/soft2036/unit5/case7/Example5_7.java
+++ src/com/soft2036/unit5/case7/Example5_7.java
@@ -8,11 +8,11 @@ package com.soft2036.unit5.case7;
  **/
 public class Example5_7 {
     public static void main(String[] args) {
-        Average aver=new Average();
-        aver.n=100.5678;
+        Average aver = new Average();
+        aver.n = 100.5678;
         double result1=aver.f();
         double result2=aver.g();
-        System.out.println("result1="+result1);
-        System.out.println("result2="+result2);
+        System.out.println("result1=" + result1);
+        System.out.println("result2=" + result2);
     }
 }
